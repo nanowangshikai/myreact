7cf56fae7bb17337ef7faf55fb4fd4a3a2173947
nano <henrywangkaikai@gmail.com>
2020-06-21 17:24:58 +0800

    Updates

---
+++ precache-manifest.b7b55ec901a1db7fc3cb70bbe58c5535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a837743f3bb9215b91e793a1660b0b",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "4ba12898d5b5c3a77825",
+    "url": "/myreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "472fc2beade2db0707a9",
+    "url": "/myreact/static/js/2.f1ca1655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact/static/js/2.f1ca1655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba12898d5b5c3a77825",
+    "url": "/myreact/static/js/main.e88b0351.chunk.js"
+  },
+  {
+    "revision": "8aa61a052fd6bc04c154",
+    "url": "/myreact/static/js/runtime-main.1b665307.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
